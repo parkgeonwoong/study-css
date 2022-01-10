ea7f7f82bf1090784cde34183c9e4cef46d64009
unknown <kweyz@daum.net>
2022-01-10 15:55:52 +0900

    requestAnimationFrame

--- mouse/README.md
+++ mouse/README.md
@@ -1,5 +1,7 @@
 # Mouse를 활용한 인터렉티브
 
+## mouse move 활용
+
 ```javascript
 window.addEventListener("mousemove", function(e) {
     console.log(e.clientX, e.clientY);
@@ -15,3 +17,18 @@ document.getElementsByClassName("className")[0];
 ```
 
 - 배열로 넘어오기 때문에 첫번째 요소인 [0]을 선택해준다.
+
+<br>
+
+## requestAnimationFrame == loop
+
+- loop라는 자연스럽게 움직이게 활용
+- 화려한 애니메이션 & 3D 컨텐츠에 활용
+
+```javascript
+function loop() {
+  console.log("계속 실행됩니다.");
+
+  window.requestAnimationFrame(loop);
+}
+```
--- mouse/app.js
+++ mouse/app.js
@@ -1,13 +1,29 @@
+let cursor_item;
+let x = 0;
+let y = 0;
+
+let mouseX = 0;
+let mouseY = 0;
+
 window.onload = function () {
   let h1 = document.getElementsByTagName("h1")[0];
-  let cursor_item = document.querySelectorAll(".cursor_item")[0];
+  cursor_item = document.querySelectorAll(".cursor_item")[0];
 
   window.addEventListener("mousemove", handleMouse, false);
 
   function handleMouse(e) {
-    h1.innerHTML = "x: " + e.clientX + " y: " + e.clientY;
-    cursor_item.style.transform =
-      "translate(" + e.clientX + "px," + e.clientY + "px)";
+    x = e.clientX;
+    y = e.clientY;
+
+    h1.innerHTML = "x: " + x + " y: " + y;
     // console.log(e.clientX, e.clientY);
   }
+  loop();
 };
+
+function loop() {
+  // console.log(x, y);
+  mouseX += x - mouseX;
+  cursor_item.style.transform = "translate(" + x + "px," + y + "px)";
+  window.requestAnimationFrame(loop);
+}
